a931c7e7a5f7428467db2ea14e16fd471fc6eacf
Rohan Mukherjee <rohanbayern@gmail.com>
2020-11-25 01:42:35 +0530

    fix: Progress bar length prints properly even on smaller wordlist

--- progress/progress.go
+++ progress/progress.go
@@ -5,36 +5,33 @@ import "fmt"
 // Progress is a struct to describe
 // the progress bar
 type Progress struct {
-	percent int64
-	cur     int64
-	total   int64
+	percent int
+	cur     int
+	total   int
 	rate    string
 	graph   string
 }
 
 // New initializes a new instance of Progress
-func (bar *Progress) New(start, total int64) {
+func (bar *Progress) New(start, total int) {
 	bar.cur = start
 	bar.total = total
 	if bar.graph == "" {
 		bar.graph = "█"
 	}
 	bar.percent = bar.getPercent()
-	for i := 0; i < int(bar.percent); i += 2 {
-		bar.rate += bar.graph // initial progress position
-	}
 }
 
-func (bar *Progress) getPercent() int64 {
-	return int64(float32(bar.cur) / float32(bar.total) * 100)
+func (bar *Progress) getPercent() int {
+	return int(float32(bar.cur) / float32(bar.total) * 100)
 }
 
 // Play increments the progress bar
-func (bar *Progress) Play(cur int64) string {
+func (bar *Progress) Play(cur int) string {
 	bar.cur = cur
-	last := bar.percent
+	bar.rate = ""
 	bar.percent = bar.getPercent()
-	if bar.percent != last && bar.percent%2 == 0 {
+	for i := 0; i < bar.percent; i+=2 {
 		bar.rate += bar.graph
 	}
 	return fmt.Sprintf("\r[%-50s]%3d%%", bar.rate, bar.percent)
