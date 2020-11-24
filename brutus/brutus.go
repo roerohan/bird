1b7efaaf868f07b08015439d9ada355126ea98a0
Rohan Mukherjee <rohanbayern@gmail.com>
2020-11-25 00:49:47 +0530

    feat: brutus implemented

---
+++ brutus/brutus.go
@@ -0,0 +1,45 @@
+package brutus
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/roerohan/bird/logger"
+)
+
+// Brute is a struct to define
+// a single job to work on
+type Brute struct {
+	URL  string
+	Word string
+}
+
+// New returns a new Brute object
+func New(url string, word string) *Brute {
+	return &Brute{
+		URL:  url,
+		Word: word,
+	}
+}
+
+// FormURL forms a URL from a brute object
+func (b *Brute) FormURL() string {
+	return fmt.Sprintf("%s/%s", b.URL, b.Word)
+}
+
+// Try tries to visit a Brute URL and checks the status code
+func (b *Brute) Try(success map[string]bool) {
+	url := b.FormURL()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		logger.Error("Error occurred while visiting " + url)
+	}
+
+	statusCode := strconv.Itoa(resp.StatusCode)
+
+	if success[statusCode] {
+		logger.Info(fmt.Sprintf("%s [Status code %s]", url, statusCode))
+	}
+}
