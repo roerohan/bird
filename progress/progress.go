face0ba6e044f23a1038aede7a645b1908333bbc
Rohan Mukherjee <rohanbayern@gmail.com>
2020-11-25 00:49:55 +0530

    feat: progress bar module

---
+++ progress/progress.go
@@ -0,0 +1,46 @@
+package progress
+
+import "fmt"
+
+// Progress is a struct to describe
+// the progress bar
+type Progress struct {
+	percent int64
+	cur     int64
+	total   int64
+	rate    string
+	graph   string
+}
+
+// New initializes a new instance of Progress
+func (bar *Progress) New(start, total int64) {
+	bar.cur = start
+	bar.total = total
+	if bar.graph == "" {
+		bar.graph = "█"
+	}
+	bar.percent = bar.getPercent()
+	for i := 0; i < int(bar.percent); i += 2 {
+		bar.rate += bar.graph // initial progress position
+	}
+}
+
+func (bar *Progress) getPercent() int64 {
+	return int64(float32(bar.cur) / float32(bar.total) * 100)
+}
+
+// Play increments the progress bar
+func (bar *Progress) Play(cur int64) {
+	bar.cur = cur
+	last := bar.percent
+	bar.percent = bar.getPercent()
+	if bar.percent != last && bar.percent%2 == 0 {
+		bar.rate += bar.graph
+	}
+	fmt.Printf("\r[%-50s]%3d%%", bar.rate, bar.percent)
+}
+
+// Finish ends printing the bar graph
+func (bar *Progress) Finish() {
+	fmt.Println()
+}
